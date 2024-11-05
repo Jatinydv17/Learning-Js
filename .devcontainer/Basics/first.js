3c18cca2bcb208f2264d7007c7276cc6d18d2e25
Jatinydv17 <128760201+Jatinydv17@users.noreply.github.com>
2024-11-05 03:09:55 +0000

    container config done

--- .devcontainer/Basics/first.js
+++ .devcontainer/Basics/first.js
@@ -1,1 +1,22 @@
 console.log("Welcome Jatin Yadav")
+
+// Variables
+const accountId = 12345
+let accEmail = "J@gmail.com"
+var accPass = "100"
+accCity = "Rewari"
+
+//const me ek baar kr diya toh krdiya , isko change nhi krskte
+
+console.table([accountId,accEmail,accPass,accCity])
+
+//let, var and khaali wala change hojaate hai
+//var me dikkat aati hai scope ki , agr 10 jagah same variable toh change hojayega
+
+/* Prefer not to use "var" */
+
+//Agar kuch abhi pta nhi hai pr baad me daal denge , toh::
+let accState;
+
+
+
